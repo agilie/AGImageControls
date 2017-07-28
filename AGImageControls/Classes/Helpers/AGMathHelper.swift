20e0c36394adf1f116fc6260a4722b183e88d903
User <liptugamichael@gmail.com>
2017-07-28 10:14:03 +0300

    Fixed AGMathHelper

--- AGImageControls/Classes/Helpers/AGMathHelper.swift
+++ AGImageControls/Classes/Helpers/AGMathHelper.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 internal struct AGTriangleStruct{
     var fisrtPoint : CGPoint
     var secondPoint : CGPoint
@@ -28,17 +29,5 @@ class AGMathHelper
     
     internal static func rad(fromDegrees value: CGFloat) -> CGFloat {
         return value /  180 * 3.14
-    }
-    
-    internal class func isTriangleContainsPoint (triangle : AGTriangleStruct, point : CGPoint) -> Bool
-    {
-        return ((point.x - triangle.fisrtPoint.x) * (triangle.fisrtPoint.y - triangle.secondPoint.y) -
-            (point.y - triangle.fisrtPoint.y) * (triangle.fisrtPoint.x - triangle.secondPoint.x)) >= 0 &&
-            
-            ((point.x - triangle.secondPoint.x) * (triangle.secondPoint.y - triangle.thirdPoint.y) -
-                (point.y - triangle.secondPoint.y) * (triangle.secondPoint.x - triangle.thirdPoint.x)) >= 0 &&
-            
-            ((point.x - triangle.thirdPoint.x) * (triangle.thirdPoint.y - triangle.fisrtPoint.y) -
-                (point.y - triangle.thirdPoint.y) * (triangle.thirdPoint.x - triangle.fisrtPoint.x)) >= 0
     }
 }
